7821820b563de2eca5b7bee96c91f090ef84ccdd
kalbers33 <kalbers33@gmail.com>
2018-11-29 22:25:27 -0800

    Change security and database to use.

--- webcomicbot/settings.py
+++ webcomicbot/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'go1dn5k=$0p5t&i$tfrseg_-bfkfd)=hzpk)z=^0zor!=qx91#'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'go1dn5k=$0p5t&i$tfrseg_-bfkfd)=hzpk)z=^0zor!=qx91#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'webcomicbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webcomicbot',
+        'USER': 'Kevin',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
