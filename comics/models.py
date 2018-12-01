81ac5af4c968d70d97016f5679e2dbeff581366d
kalbers33 <kalbers33@gmail.com>
2018-11-30 23:27:35 -0800

    Update the model to what I think I want for the model.
    I might have this new model do the actual scraping, or call into it. I
    am not sure if that is good design.
    
    I will need to do more research on scrapy to see if this is set up
    correctly.

--- comics/models.py
+++ comics/models.py
@@ -1,18 +1,29 @@
 from django.db import models
 
 from django.utils import timezone
-from dynamic_scraper.models import Scraper, SchedulerRuntime
-from scrapy_djangoitem import DjangoItem
 
 import datetime
 
 
+class ComicStripScraper(models.Model):
+    DEFAULT = 'default'
+    SCRAPER_CHOICES = (
+            (DEFAULT, DEFAULT),
+            )
+    scraper = models.CharField(
+            max_length=100,
+            choices = SCRAPER_CHOICES,
+            default=DEFAULT)
+
+    def __str__(self):
+        return self.scraper
+
 class ComicStrip(models.Model):
+
     name = models.CharField(max_length=200)
     base_url = models.URLField(max_length=200)
     rss_feed = models.URLField(max_length=500, default='')
-    scraper = models.ForeignKey(Scraper, blank=True, null=True, on_delete=models.SET_NULL)
-    scraper_runtime = models.ForeignKey(SchedulerRuntime, blank=True, null=True, on_delete=models.SET_NULL)
+    scraper = models.ForeignKey(ComicStripScraper, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -26,15 +37,10 @@ class Comic(models.Model):
     comic_page_url = models.URLField(max_length=500, default='')
     comic_url = models.URLField(max_length=500)
     alt_comic_url = models.URLField(max_length=500, default='')
-    checker_runtime = models.ForeignKey(SchedulerRuntime, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.title
 
     def was_published_recently(self):
                 return self.date >= timezone.now().date() - datetime.timedelta(days=1)
-
-class ComicItem(DjangoItem):
-    django_model = Comic
-
 
