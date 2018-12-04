25cf07ac3e842ff38c670c41bd62f92f4841cf76
kalbers33 <kalbers33@gmail.com>
2018-12-03 21:01:11 -0800

    Update to use the new url paths.

--- comics/urls.py
+++ comics/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import path, re_path, include
 
 from . import views
 
 app_name='comics'
 urlpatterns = [
-    url('', views.index, name='index'),
-    url('(?P<year>[0-9]+)/(?P<month>[0-9]+)/(?P<day>[0-9]+)/date/', views.date, name='date'),
-    url('(?P<comic_strip_id>[0-9]+)/comic_strip/', views.comic_strip, name='comic_strip'),
-    url('(?P<comic_id>[0-9]+)/comic/', views.comic, name='comic'),
-    url('(?P<comic_strip_id>[0-9]+)/update/', views.update_comics, name='update_comics'),
+    path('', views.index, name='index'),
+    re_path('(?P<year>[0-9]+)/(?P<month>[0-9]+)/(?P<day>[0-9]+)/date/', views.date, name='date'),
+    re_path('(?P<comic_strip_id>[0-9]+)/comic_strip/', views.comic_strip, name='comic_strip'),
+    re_path('(?P<comic_id>[0-9]+)/comic/', views.comic, name='comic'),
+    re_path('(?P<comic_strip_id>[0-9]+)/update/', views.update_comics, name='update_comics'),
     ]
