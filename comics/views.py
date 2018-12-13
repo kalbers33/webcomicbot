036169781054f8ee50e56bd4421f11d30827643f
kalbers33 <kalbers33@gmail.com>
2018-12-12 22:10:18 -0800

    Order comics by decending date on comic page.
    Show the date here (since the title does not always have this).

--- comics/views.py
+++ comics/views.py
@@ -28,7 +28,7 @@ def date(request, year, month, day):
 
 def comic_strip(request, comic_strip_id, message=None):
     comic_strip = get_object_or_404(ComicStrip, id=comic_strip_id)
-    all_comics = comic_strip.comic_set.all()
+    all_comics = comic_strip.comic_set.all().order_by('-date')
     context = {
         'comic_strip' : comic_strip,
         'comic_list': all_comics,
